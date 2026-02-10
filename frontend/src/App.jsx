f32ab1639e6a0681772e7b0e68d17de67fbe3d52
Siddhartha Dey Chetia <excutioner01@gmail.com>
2026-02-11 02:51:23 +0530

    added style css for video page and updated the app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
-import { useState } from "react";
+import VideoPage from "./pages/VideoPage";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -11,16 +12,24 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Auth routes (NO layout) */}
+        {/* Auth */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
 
-        {/* App routes (WITH layout) */}
+        {/* App */}
         <Route
           path="/"
           element={
             <Layout searchTerm={searchTerm} setSearchTerm={setSearchTerm}>
               <Home searchTerm={searchTerm} />
+            </Layout>
+          }
+        />
+        <Route
+          path="/video/:id"
+          element={
+            <Layout searchTerm={searchTerm} setSearchTerm={setSearchTerm}>
+              <VideoPage />
             </Layout>
           }
         />
--- frontend/src/components/VideoCard.jsx
+++ frontend/src/components/VideoCard.jsx
@@ -1,18 +1,16 @@
+import { useNavigate } from "react-router-dom";
 import "../styles/video.css";
 
 const VideoCard = ({ video }) => {
-  return (
-    <div className="video-card">
-      <img
-        src={video.thumbnailUrl}
-        alt={video.title}
-        className="video-thumb"
-      />
+  const navigate = useNavigate();
 
+  return (
+    <div className="video-card" onClick={() => navigate(`/video/${video.videoId}`)}>
+      <img src={video.thumbnailUrl} alt={video.title} className="video-thumb" />
       <div className="video-info">
         <h4 className="video-title">{video.title}</h4>
         <p className="video-channel">{video.channelName}</p>
-        <p className="video-views">{video.views} views</p>
+        <p className="video-views">{video.views.toLocaleString()} views</p>
       </div>
     </div>
   );
