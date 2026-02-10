1ef1f806a050ef2c65b92080a10b80387925414a
Siddhartha Dey Chetia <excutioner01@gmail.com>
2026-02-10 21:41:16 +0530

    implement signup and login APIs

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
 connectDB();
 
 const app = express();
-
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
 const PORT = process.env.PORT || 3500;
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
