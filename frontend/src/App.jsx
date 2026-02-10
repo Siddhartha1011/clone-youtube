aa8ad26faeff9d5802c8bec453a188270bbd3a10
Siddhartha Dey Chetia <excutioner01@gmail.com>
2026-02-10 22:00:31 +0530

    create login and registration pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+
 const App = () => {
   return (
-    <div>
-      <h1>YouTube Clone</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
